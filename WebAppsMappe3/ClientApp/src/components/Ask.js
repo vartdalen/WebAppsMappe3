0c54373154378a7d21061526b8067e214238847b
Viktor Vartdal Johansen <viktor.v.johansen@hotmail.com>
2018-11-11 16:19:57 +0100

    ikke fungerende react validation med react-bootstrap

--- WebAppsMappe3/ClientApp/src/components/Ask.js
+++ WebAppsMappe3/ClientApp/src/components/Ask.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/lib/Form';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import InputGroup from 'react-bootstrap/lib/InputGroup';
 
 export class Ask extends Component {
 	displayName = Ask.name
@@ -13,17 +17,105 @@ export class Ask extends Component {
 			questions: [],
 			loading: true
 		};
+		this.fetchQuestionsTable = this.fetchQuestionsTable.bind(this);
+		this.submitQuestion = this.submitQuestion.bind(this);
+		this.deleteQuestion = this.deleteQuestion.bind(this);
 		this.insertQuestionToInput = this.insertQuestionToInput.bind(this);
 		this.insertAskerToInput = this.insertAskerToInput.bind(this);
 		this.insertAnswerToInput = this.insertAnswerToInput.bind(this);
 		this.insertReplierToInput = this.insertReplierToInput.bind(this);
-		this.submitQuestion = this.submitQuestion.bind(this);
-		this.deleteQuestion = this.deleteQuestion.bind(this);
+		this.validateName = this.validateName.bind(this);
+		this.validateSentence = this.validateSentence.bind(this);
 		this.voteUp = this.voteUp.bind(this);
 		this.voteDown = this.voteDown.bind(this);
-		this.fetchQuestionsTable = this.fetchQuestionsTable.bind(this);
 
 		this.fetchQuestionsTable();
+	}
+
+	fetchQuestionsTable() {
+		fetch('api/Questions')
+			.then(response => response.json())
+			.then(data => {
+				this.setState({ questions: data, loading: false, question: '', asker: '', answer: '', replier: '' });
+			});
+	}
+
+	submitQuestion(event) {
+		event.preventDefault();
+		fetch('api/Questions', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ 'question': this.state.question, 'asker': this.state.asker })
+		}).then(() => this.fetchQuestionsTable());
+	}
+
+	deleteQuestion(dbIndex) {
+		fetch('api/Questions/' + dbIndex, {
+			method: 'DELETE',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ 'id': dbIndex })
+		}).then(() => this.fetchQuestionsTable());
+	}
+
+	submitAnswer(event, index, dbIndex) {
+		event.preventDefault();
+		const questionsAnswered = this.state.questions.slice()
+		questionsAnswered[index].answer = this.state.answer
+		questionsAnswered[index].replier = this.state.replier
+
+		fetch('api/Questions/' + dbIndex, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				'id': dbIndex,
+				'question': questionsAnswered[index].question,
+				'asker': questionsAnswered[index].asker,
+				'answer': questionsAnswered[index].answer,
+				'replier': questionsAnswered[index].replier,
+				'voteUp': questionsAnswered[index].voteUp,
+				'voteDown': questionsAnswered[index].voteDown
+			})
+		}).then(() => this.fetchQuestionsTable());
+	}
+
+	displayAnswer(replier) {
+		if (replier === '' || replier === null) {
+			return 'div-highlight container-form hidden';
+		} else {
+			return 'div-highlight container-form show';
+		}
+	}
+
+	insertQuestionToInput(event) {
+		this.setState({ question: event.target.value });
+	}
+
+	insertAskerToInput(event) {
+		this.setState({ asker: event.target.value });
+	}
+
+	insertAnswerToInput(event) {
+		this.setState({ answer: event.target.value });
+	}
+
+	insertReplierToInput(event) {
+		this.setState({ replier: event.target.value });
+	}
+
+	validateName(name) {
+		const length = name.value.length;
+		if (length > 10) return 'success';
+		else if (length > 5) return 'warning';
+		else if (length > 0) return 'error';
+		return null;
+	}
+
+	validateSentence(sentence) {
+		const length = sentence.value.length;
+		if (length > 10) return 'success';
+		else if (length > 5) return 'warning';
+		else if (length > 0) return 'error';
+		return null;
 	}
 
 	voteUp(index, dbIndex) {
@@ -66,76 +158,6 @@ export class Ask extends Component {
 		this.setState({ questions: questionsDownVoted });
 	}
 
-	submitQuestion(event) {
-		event.preventDefault();
-		fetch('api/Questions', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ 'question': this.state.question, 'asker': this.state.asker })
-		}).then(() => this.fetchQuestionsTable());
-	}
-
-	deleteQuestion(dbIndex) {
-		fetch('api/Questions/' + dbIndex, {
-			method: 'DELETE',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ 'id': dbIndex })
-		}).then(() => this.fetchQuestionsTable());
-	}
-
-	submitAnswer(event, index, dbIndex) {
-		event.preventDefault();
-		const questionsAnswered = this.state.questions.slice()
-		questionsAnswered[index].answer = this.state.answer
-		questionsAnswered[index].replier = this.state.replier
-
-		fetch('api/Questions/' + dbIndex, {
-			method: 'PUT',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				'id': dbIndex,
-				'question': questionsAnswered[index].question,
-				'asker': questionsAnswered[index].asker,
-				'answer': questionsAnswered[index].answer,
-				'replier': questionsAnswered[index].replier,
-				'voteUp': questionsAnswered[index].voteUp,
-				'voteDown': questionsAnswered[index].voteDown
-			})
-		}).then(() => this.fetchQuestionsTable());
-	}
-
-	insertQuestionToInput(event) {
-		this.setState({ question: event.target.value });
-	}
-
-	insertAskerToInput(event) {
-		this.setState({ asker: event.target.value });
-	}
-
-	insertAnswerToInput(event) {
-		this.setState({ answer: event.target.value });
-	}
-
-	insertReplierToInput(event) {
-		this.setState({ replier: event.target.value });
-	}
-
-	setupAnswer(replier) {
-		if (replier === '' || replier === null) {
-			return 'div-highlight container-form hidden';
-		} else {
-			return 'div-highlight container-form show';
-		}
-	}
-
-	fetchQuestionsTable() {
-		fetch('api/Questions')
-			.then(response => response.json())
-			.then(data => {
-				this.setState({ questions: data, loading: false, question: '', asker: '', answer: '', replier: ''});
-			});
-	}
-
 	render() {
 		let contents = this.state.loading
 		if (contents) { return (<p><em>Loading...</em></p>); }
@@ -166,39 +188,47 @@ export class Ask extends Component {
 								</div>
 								<div id={'collapse' + index} className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
 									<div className="panel-body">
-										<div className={this.setupAnswer(question.replier)}>
+										<div className={this.displayAnswer(question.replier)}>
 											<label htmlFor="answer">{question.replier}'s reply: </label><br />
 											{question.answer}
-										</div><br />
-										<form onSubmit={(e) => { this.submitAnswer(e, index, question.id) }}>
+										</div>
+										<Form onSubmit={(e) => { this.submitAnswer(e, index, question.id) }}>
 											<label htmlFor="input-replier">Reply to {question.asker}:</label><br />
-											<div className="input-group">
-												<div className="input-group-addon"><span className="glyphicon glyphicon-user" aria-hidden="true"></span></div>
-												<input className="form-control" type="text" name="replier" id="input-replier" placeholder="Insert name here" value={this.state.replier} onChange={this.insertReplierToInput} />
-											</div><br />
-											<div className="input-group">
-												<div className="input-group-addon"><span className="glyphicon glyphicon-info-sign" aria-hidden="true"></span></div>
-												<input type="text" className="form-control" name="answer" id="answer" placeholder="Insert answer here" value={this.state.answer} onChange={this.insertAnswerToInput} />
-											</div>
+											<FormGroup validationState={() => { this.validateName(this.state.replier) }}>
+												<InputGroup>
+													<InputGroup.Addon><span className="glyphicon glyphicon-user"></span></InputGroup.Addon>
+													<FormControl type="text" name="replier" id="input-replier" placeholder="Insert name here" value={this.state.replier} onChange={this.insertReplierToInput} />
+												</InputGroup>
+											</FormGroup>
+											<FormGroup validationState={() => { this.validateSentence(this.state.answer) }}>
+												<InputGroup>
+													<InputGroup.Addon><span className="glyphicon glyphicon-info-sign"></span></InputGroup.Addon>
+													<FormControl type="text" name="answer" id="input-answer" placeholder="Insert answer here" value={this.state.answer} onChange={this.insertAnswerToInput} />
+												</InputGroup>
+											</FormGroup>
 											<button type="submit" className="btn btn-primary">Submit answer</button>
-										</form>
+										</Form>
 									</div>
 								</div>
 							</div>
 						)}
 					</div>
-					<form className="panel-body div-highlight container-form" onSubmit={this.submitQuestion}>
+					<Form className="div-highlight container-form" onSubmit={this.submitQuestion}>
 						<label htmlFor="input-asker">Ask a question:</label><br />
-						<div className="input-group">
-							<div className="input-group-addon"><span className="glyphicon glyphicon-user" aria-hidden="true"></span></div>
-							<input className="form-control" type="text" name="asker" id="input-asker" placeholder="Insert name here" value={this.state.asker} onChange={this.insertAskerToInput} />
-						</div><br />
-						<div className="input-group">
-							<div className="input-group-addon"><span className="glyphicon glyphicon-question-sign" aria-hidden="true"></span></div>
-							<input className="form-control" type="text" name="question" id="input-question" placeholder="Insert question here" value={this.state.question} onChange={this.insertQuestionToInput} />
-						</div>
+						<FormGroup>
+							<InputGroup>
+								<InputGroup.Addon><span className="glyphicon glyphicon-user"></span></InputGroup.Addon>
+								<FormControl type="text" name="asker" id="input-asker" placeholder="Insert name here" value={this.state.asker} onChange={this.insertAskerToInput} validationState={() => { this.validateName(this.state.asker) }}/>
+							</InputGroup>
+						</FormGroup>
+						<FormGroup>
+							<InputGroup>
+								<InputGroup.Addon><span className="glyphicon glyphicon-question-sign"></span></InputGroup.Addon>
+								<FormControl type="text" name="question" id="input-question" placeholder="Insert question here" value={this.state.question} onChange={this.insertQuestionToInput} validationState={() => { this.validateSentence(this.state.question) }}/>
+							</InputGroup>
+						</FormGroup>
 						<button type="submit" className="btn btn-primary">Submit question</button>
-					</form>
+					</Form>
 				</div>
 			);
 		}
